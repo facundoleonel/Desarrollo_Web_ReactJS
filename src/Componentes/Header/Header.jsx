c8f02b5cba6967dce55ad6a4ca91a7916f6a5412
facundoleonel <facundopanozzo@gmail.com>
2023-08-17 16:05:32 -0300

    Project

---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+//Router: son las rutas
+import { BrowserRouter, Route, Routes, link } from "react-router-dom";
+
+function Header (){
+    return(
+        <h1>Header</h1>
+    );
+}
+
+export {Header};

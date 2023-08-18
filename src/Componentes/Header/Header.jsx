428502500d4ed28fef3f4fe4e33ac646cc183d45
facundoleonel <facundopanozzo@gmail.com>
2023-08-17 22:09:47 -0300

    components

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,10 +1,26 @@
-
+// imports de bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 //Router: son las rutas
-import { BrowserRouter, Route, Routes, link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function Header (){
-    return(
-        <h1>Header</h1>
+    return (
+      <>
+        <Navbar collapseOnSelect expand='lg' bg="dark" data-bs-theme="dark">
+            <Container>
+            <Navbar.Brand href="/">UNER</Navbar.Brand>
+            <Navbar.Toggle aria-controls='algo'/>
+            <Navbar.Collapse id='algo'>
+                {/* <Nav className="me-auto">            
+                <Nav.Link as={Link} to='/institucional'>Institucional</Nav.Link>
+                <Nav.Link as={Link} to='/contacto'>Contacto</Nav.Link>
+                </Nav> */}
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+      </>
     );
 }
 

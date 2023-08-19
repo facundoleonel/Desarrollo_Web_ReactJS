ffa54a740a8501c50ac67891716204d07d247abe
facundoleonel <facundopanozzo@gmail.com>
2023-08-18 23:26:29 -0300

    useState

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 
 
 function Inicio (){
+
+    // const [tema, setTema] = useState("");
+    // const [noticias, setNoticias] = useState([]);
+
+    // useEffect(() =>{
+    //     if(tema)
+    // });
+
     return (
         <div className='container mt-5'>
             <h1>Inicio</h1>
+            
+
         </div>
     );
 }

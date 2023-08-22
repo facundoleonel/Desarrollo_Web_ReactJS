6c1c9922f38d010adeab48ecc3b5b6f977bba277
facundoleonel <facundopanozzo@gmail.com>
2023-08-22 19:29:37 -0300

    Institucion

--- src/Pages/Institucion.jsx
+++ src/Pages/Institucion.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export const Institucion = () => {
   return (
     <>
-    <h1 className="title text-center">Institucion</h1>
-    <div className="container px-4 text-center">
+      <h2 className="title text-center">Institucion</h2>
+      <div className="container px-4">
         <div className="row gx-5">
-            <div className="col">
-            <div className="p-3">Custom column padding</div>
+          <div className="col">
+            <div className="p-3">
+              <h4 className="text-center">Valores</h4>
+              <h5>Calidad académica</h5>
+              <p>
+                Búsqueda permanente de un elevado nivel académico científico y
+                compromiso con la generación de nuevos conocimientos y su
+                transferencia.
+              </p>
+              <h5>Honestidad y transparencia</h5>
+              <p>
+                La institución promueve la honestidad en el desempeño de todos
+                sus miembros y la transparencia en la gestión.
+              </p>
             </div>
-            <div className="col">
+          </div>
+          <div className="col">
             <div className="p-3">Custom column padding</div>
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-);
-}
+  );
+};

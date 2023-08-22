1a949a4c220554b982f85305b118d85765ec4490
facundoleonel <facundopanozzo@gmail.com>
2023-08-22 19:38:36 -0300

    Institucion

--- src/Pages/Institucion.jsx
+++ src/Pages/Institucion.jsx
@@ -20,6 +20,34 @@ export const Institucion = () => {
                 La institución promueve la honestidad en el desempeño de todos
                 sus miembros y la transparencia en la gestión.
               </p>
+              <h5>Respeto</h5>
+              <p>
+                La institución promueve el respeto por los derechos y la
+                dignidad de las personas y por el ambiente.
+              </p>
+              <h5>Libertad académica</h5>
+              <p>Libertad de enseñar y aprender en un ámbito democrático.</p>
+              <h5>Compromiso social y regional</h5>
+              <p>
+                El compromiso social y regional se manifiesta en que las
+                carreras de grado y posgrado, los planes de estudio, los
+                contenidos de los programas así como los proyectos de
+                investigación y extensión contemplen el desarrollo social y
+                regional. Entendiendo el espacio regional en su interrelación
+                con lo local, lo nacional y lo global.
+              </p>
+              <h5>Democracia</h5>
+              <p>
+                La democracia como modelo de gobierno de la institución que
+                promueve la participación de todos sus miembros, como modo de
+                pensar y actuar y como valor a transmitir y fortalecer en la
+                comunidad educativa y en la sociedad.
+              </p>
+              <h5>Innovación</h5>
+              <p>
+              Promover una actitud proactiva y emprendedora, generando un ambiente propicio 
+              para ello.
+              </p>
             </div>
           </div>
           <div className="col">

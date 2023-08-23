e3dcf6ad81a00da706d324e1fc7dd665c2729740
facundoleonel <facundopanozzo@gmail.com>
2023-08-23 17:44:57 -0300

    terminado Institucion

--- src/Pages/Institucion.jsx
+++ src/Pages/Institucion.jsx
@@ -3,31 +3,31 @@ import React from "react";
 export const Institucion = () => {
   return (
     <>
-      <h2 className="title text-center">Institucion</h2>
-      <div className="container px-4">
+      <h5 className="text-center mt-3">LA INSTITUCIÓN</h5>
+      <div className="container px-4 mt-5">
         <div className="row gx-5">
           <div className="col">
             <div className="p-3">
-              <h4 className="text-center">Valores</h4>
-              <h5>Calidad académica</h5>
+              <h4 className="text-center title">Valores</h4>
+              <h5 className="title">Calidad académica</h5>
               <p>
                 Búsqueda permanente de un elevado nivel académico científico y
                 compromiso con la generación de nuevos conocimientos y su
                 transferencia.
               </p>
-              <h5>Honestidad y transparencia</h5>
+              <h5 className="title">Honestidad y transparencia</h5>
               <p>
                 La institución promueve la honestidad en el desempeño de todos
                 sus miembros y la transparencia en la gestión.
               </p>
-              <h5>Respeto</h5>
+              <h5 className="title">Respeto</h5>
               <p>
                 La institución promueve el respeto por los derechos y la
                 dignidad de las personas y por el ambiente.
               </p>
-              <h5>Libertad académica</h5>
+              <h5 className="title">Libertad académica</h5>
               <p>Libertad de enseñar y aprender en un ámbito democrático.</p>
-              <h5>Compromiso social y regional</h5>
+              <h5 className="title">Compromiso social y regional</h5>
               <p>
                 El compromiso social y regional se manifiesta en que las
                 carreras de grado y posgrado, los planes de estudio, los
@@ -36,23 +36,23 @@ export const Institucion = () => {
                 regional. Entendiendo el espacio regional en su interrelación
                 con lo local, lo nacional y lo global.
               </p>
-              <h5>Democracia</h5>
+              <h5 className="title">Democracia</h5>
               <p>
                 La democracia como modelo de gobierno de la institución que
                 promueve la participación de todos sus miembros, como modo de
                 pensar y actuar y como valor a transmitir y fortalecer en la
                 comunidad educativa y en la sociedad.
               </p>
-              <h5>Innovación</h5>
+              <h5 className="title">Innovación</h5>
               <p>
                 Promover una actitud proactiva y emprendedora, generando un
                 ambiente propicio para ello.
               </p>
             </div>
           </div>
-          <div className="col">
+          <div className="col" style={{backgroundColor:"#D5DBDB"}}>
             <div className="p-3">
-              <h4 className="text-center">Misión</h4>
+              <h4 className="text-center title">Misión</h4>
               <p>
                 La Facultad de Ciencias de la Administración promueve la
                 docencia, la investigación y la extensión para la generación y
@@ -61,8 +61,17 @@ export const Institucion = () => {
                 compromiso con la calidad académica, los valores democráticos y
                 la autonomía en la toma de decisiones.
               </p>
-            
-              <h4 className="text-center">Visión</h4>
+              <p  style={{ fontWeight:700 }}>Sus principales funciones son promover:</p>
+              <ul>
+                <li>La formación integral del estudiante.</li>
+                <li>La mejora continua del proceso de enseñanza y aprendizaje de grado y posgrado.</li>
+                <li>El desarrollo científico mediante la producción de nuevos conocimientos.</li>
+                <li>La generación de espacios de formación continua para los graduados.</li>
+                <li>La cooperación y la vinculación con otras instituciones del sistema educativo y de la sociedad.</li>
+                <li>El desarrollo de competencias emprendedoras.</li>
+                <li>Las actividades socio-culturales.</li>
+              </ul>
+              <h4 className="text-center title">Visión</h4>
               <p>
                 La Facultad de Ciencias de la Administración es un referente
                 regional en la formación de grado y posgrado, reconocida por su

bef167f7aeed2c554c5065d0a0ba0f939cc48329
marcelo <mrromero@teco.com.ar>
2023-08-23 11:18:38 -0300

    Movemos test a una carpeta

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Assets/css/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './Tests/reportWebVitals';
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';

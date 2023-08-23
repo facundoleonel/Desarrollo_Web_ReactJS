2a713db7eed8cd58f8d3f2d97359b19e0fdbee41
facundoleonel <facundopanozzo@gmail.com>
2023-08-23 19:31:09 -0300

    contacto formulario

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Form, Button, Card, Table} from "react-bootstrap" ;
 
 export const Contacto = () => {
+
+  const [tab, setTab]=useState({nombre:'', correo:'', mensaje:''});
+
+  function Informacion(){
+    alert(JSON.stringify(tab));
+}
+
   return (
     <>
       <h5 className="text-center mt-3">Contacto</h5> <hr />
@@ -18,6 +27,60 @@ export const Contacto = () => {
             <p>oficinacyt.fcad@uner.edu.ar</p>
             <h5 className="title">Oficina de Pasantías</h5>
             <p>pasantias.fcad@uner.edu.ar</p>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Card>
+              <Card.Body>
+                <Card.Title>Envianos tu consulta</Card.Title>
+
+                <Form onSubmit={Informacion}>
+                  <Form.Group className="mb-3" controlId="formBasicNombre">
+                    <Form.Label>Nombre y Apellido</Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={(e) =>
+                        setTab({ ...tab, nombre: e.target.value })
+                      }
+                    />
+                    <Form.Text className="text-muted">
+                      No compartiremos tu correo.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicCorreo">
+                    <Form.Label>Correo Electrónico</Form.Label>
+                    <Form.Control
+                      type="email"
+                      onChange={(e) =>
+                        setTab({ ...tab, correo: e.target.value })
+                      }
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicMensaje">
+                    <Form.Label>Mensaje</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={5}
+                      onChange={(e) =>
+                        setTab({
+                          ...tab,
+                          mensaje: e.target.value,
+                        })
+                      }
+                    />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Enviar
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
           </div>
         </div>
       </div>

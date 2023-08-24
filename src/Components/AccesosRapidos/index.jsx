6be89a46e022382d0dbb7d6ca5157db0f1a0430f
marcelo <mrromero@teco.com.ar>
2023-08-24 11:28:04 -0300

    cambia nombre a variable - archivo

--- src/Components/AccesosRapidos/index.jsx
+++ src/Components/AccesosRapidos/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import LinkThree from "./../../Assets/jsons/linkThree.json";
+import { Col, Row } from "react-bootstrap";
+import LinksFcad from "./../../Assets/jsons/links-fcad.json";
 export const AccesosRapidos = () => {
   return (
       <Row className="mt-3 mb-3" style={{rowGap: '10px'}}>
-        {LinkThree.map(({ name, url }, k) => (
+        {LinksFcad.map(({ name, url }, k) => (
           <Col sm={3} key={k}>
             <a href={url} target="_blank" rel="noreferrer" className="elementor-container">
               {name}

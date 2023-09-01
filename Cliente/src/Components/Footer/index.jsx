3823b2b2f1dd1e2d6d88fc6ceaaf4d158b24227f
facundoleonel <facundopanozzo@gmail.com>
2023-09-01 19:26:06 -0300

    footer

--- Cliente/src/Components/Footer/index.jsx
+++ Cliente/src/Components/Footer/index.jsx
@@ -5,9 +5,20 @@ function Footer() {
     <footer>
       <div className="container text-center">
         <div className="row align-items-start">
-          <div className="col">Marcelo Romero</div>
-          <div className="col">Gabriela Bejarano</div>
-          <div className="col">Facundo Leonel Panozzo</div>
+          <div className="col">
+          <h6>Enlaces útiles</h6> <hr/>
+          </div>
+          <div className="col">
+          <h6>Acceso a Redes</h6> <hr/>
+          </div>
+          <div className="col">
+            <h6>Facultad de Ciencias de la Administración</h6> <hr/>
+            <p>
+              Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de Entre
+              Ríos Teléfono: (+54) (345) 4231400 – Fax: (+54) (345) 4231410 |
+              E-mail.: informes.fcad@uner.edu.ar
+            </p>
+          </div>
         </div>
       </div>
       <div className="footer text-center">
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 
+//mi app del servidor 
+const app = express();
+
+const port = process.env.PUERTO || 3005;
+app.get('/', (req, res) => {
+res.type('text/plain');
+res.status(200);
+res.send('Hola soy una app Express!');
+});
+app.listen(port, () => console.log(`Express started on http://localhost:${port}`));

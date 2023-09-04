0648d838d112a3b0c5ed95a93b80a0580e7eb1a1
facundoleonel <facundopanozzo@gmail.com>
2023-09-04 13:27:59 -0300

    footer

--- Cliente/src/Components/Footer/index.jsx
+++ Cliente/src/Components/Footer/index.jsx
@@ -1,4 +1,6 @@
+import { Container, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./footer.css";
+import Listenlaces from "./../../Assets/jsons/enlaces-utiles.json";
 
 function Footer() {
   return (
@@ -6,13 +8,47 @@ function Footer() {
       <div className="container text-center">
         <div className="row align-items-start">
           <div className="col">
-          <h6>Enlaces útiles</h6> <hr/>
+            <h6>Enlaces útiles</h6> <hr />
+            <Container>
+              <Row>
+                <Col>
+                  <ListGroup
+                    className="elementor-tablet"
+                    style={{ backgroundColor: "#101318" }}
+                  >
+                    {Listenlaces.map(({ name, url }, k) => (
+                      <ListGroup.Item key={k}>
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="elementor-enlace"
+                        >
+                          {name}
+                        </a>
+                      </ListGroup.Item>
+                    ))}
+                  </ListGroup>
+                  <ul></ul>
+                </Col>
+              </Row>
+            </Container>
           </div>
           <div className="col">
-          <h6>Acceso a Redes</h6> <hr/>
+            <h6>Acceso a Redes</h6> <hr />
+            <div class="container px-4 text-center">
+              <div class="row gx-2">
+                <div class="col">
+                  <div class="p-3">Custom column padding</div>
+                </div>
+                <div class="col">
+                  <div class="p-3">Custom column padding</div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col">
-            <h6>Facultad de Ciencias de la Administración</h6> <hr/>
+            <h6>Facultad de Ciencias de la Administración</h6> <hr />
             <p>
               Monseñor Tavella 1424. Concordia. CP(3200). | Provincia de Entre
               Ríos Teléfono: (+54) (345) 4231400 – Fax: (+54) (345) 4231410 |

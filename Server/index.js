21a7fd7fe25b27587102649fb484205f1733e93b
marcelo <mrromero@teco.com.ar>
2023-09-04 22:22:35 -0300

    agrega un get json

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,35 @@
-const express = require('express');
+const express = require("express");
 
-//mi app del servidor 
+//mi app del servidor
 const app = express();
 
 const port = process.env.PUERTO || 3005;
-app.get('/', (req, res) => {
-res.type('text/plain');
-res.status(200);
-res.send('Hola soy una app Express!');
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.status(200);
+  res.send("Hola soy una app Express!");
 });
-app.listen(port, () => console.log(`Express started on http://localhost:${port}`));
+
+app.get("/carreras", (req, res) => {
+  res.type("application/json");
+  res.status(200);
+  res.send([
+    {
+      titulo: "Tecnicatura desarrollo web",
+      duracion: 3,
+    },
+    {
+      titulo: "Turismo",
+      duracion: 5,
+    },
+    {
+      titulo: "Administracion",
+      duracion: 5,
+    },
+  ]);
+});
+
+app.listen(port, () =>
+  console.log(`Express started on http://localhost:${port}`)
+);

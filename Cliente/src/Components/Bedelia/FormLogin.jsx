1a430af9b9c868adaeb451a7234e52539625a5b8
marcelo <mrromero@teco.com.ar>
2023-10-19 15:55:48 -0300

    fix imports

--- Cliente/src/Components/Bedelia/FormLogin.jsx
+++ Cliente/src/Components/Bedelia/FormLogin.jsx
@@ -3,7 +3,7 @@ import { Button, Form } from "react-bootstrap";
 
 import { useNavigate } from "react-router-dom";
 import { userContext } from "../../Layout";
-import { setLocalUser } from "../../temp/simulador";
+import { setLocalUser } from "./../../Helpers/localstorage";
 import { buscarUsuario } from "../../Helpers/usuario";
 import { ShowNotification, ShowNotificationError } from "../../Helpers/utils";
 

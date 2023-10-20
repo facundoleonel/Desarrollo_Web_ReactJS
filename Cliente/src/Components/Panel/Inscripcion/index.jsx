1481dce7b65e5e361018d47003af1d203325fe5b
marcelo <mrromero@teco.com.ar>
2023-10-20 14:46:15 -0300

    init inscripcion

---
+++ Cliente/src/Components/Panel/Inscripcion/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+
+export const Inscripcion = () => {
+  const [page, setPage] = useState(0);
+
+  const handleNext = ()=>{
+    setPage( page + 1 )
+  }
+
+  return (
+    <>
+    { page === 0 && 
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Seleccionar estudiante y carrera</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <button onClick={handleNext}>Continuar</button>
+          </Card.Body>
+        </Card>
+    }
+    { page === 1 && 
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Seleccionar Materias</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            {/* <button onClick={handleNext}>Continuar</button> */}
+          </Card.Body>
+        </Card>
+    }
+    </>
+  )
+};
--- Cliente/src/Pages/Panel/Inscripciones.jsx
+++ Cliente/src/Pages/Panel/Inscripciones.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container } from 'react-bootstrap'
 import { SectionTitle } from '../../Layout/SectionTitle'
+import { Inscripcion } from '../../Components/Panel/Inscripcion'
 
 export const Inscripciones = () => {
   return (
@@ -8,6 +9,7 @@ export const Inscripciones = () => {
       <Container className="mt-5 mb-4">
         <Col xs={12}>
           <SectionTitle title='Panel Inscripciones' />
+          <Inscripcion />
         </Col>
       </Container>
     </>

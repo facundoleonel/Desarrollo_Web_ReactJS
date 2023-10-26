8c61ff077a963cd66561e6be68ba94657019fadd
facundoleonel <facundopanozzo@gmail.com>
2023-10-25 21:08:01 -0300

    Dashboard

--- Cliente/src/Pages/Panel/index.jsx
+++ Cliente/src/Pages/Panel/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, CardGroup, Col, Container, Row } from "react-bootstrap";
 import { SectionTitle } from "../../Layout/SectionTitle";
 import Articulo from "../../Components/Home/Articulos/Articulo";
 import { userContext } from "../../Layout";
@@ -17,15 +17,71 @@ export const Panel = () => {
     };
     loadRoutes();
 
-    setTitle(`Bienvenido ${currentUser.nombre} ${currentUser.apellido}`)
-
+    setTitle(`Bienvenido ${currentUser.nombre} ${currentUser.apellido}`);
   }, [currentUser]);
   return (
     <Container>
       <Row>
         <Col>
           <SectionTitle title={title} />
-          <p>Estos son tus accesos directos disponibles</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card style={{ width: "18rem" }} bg='success'>
+            <Card.Body>
+              <Card.Title>Inscripciones</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: "18rem" }} bg='danger'>
+            <Card.Body>
+              <Card.Title>Estudiantes</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: "18rem" }} bg='info'>
+            <Card.Body>
+              <Card.Title>Carreras</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: "18rem" }} bg='warning'>
+            <Card.Body>
+              <Card.Title>Materias</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
       <Row>
